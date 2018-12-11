3a52dcc3e00fdd4ffe32f8faa9b1a12c406b1aa6
Ashley Schoonover <aschoo@umich.edu>
2018-12-11 14:54:00 -0500

    added foreign key constraints to sql and ran it

--- sql_data/input/hospitals.sql
+++ sql_data/input/hospitals.sql
@@ -8,7 +8,8 @@
 -- 1.0 Setup. Delete tables after every build iteration.
 --
 SET FOREIGN_KEY_CHECKS=0;
-DROP TABLE IF EXISTS city, state, hospital, temp_hospital, hospital_ownership, hospital_quality_score, hospital_pricing;
+DROP TABLE IF EXISTS city, state, zip_code, hospital, temp_hospital, hospital_ownership, hospital_quality_score, hospital_pricing,
+temp_pricing, charge_amount, pricing;
 SET FOREIGN_KEY_CHECKS=1;
 
 --
@@ -90,7 +91,27 @@ INTO TABLE state
     IGNORE 1 LINES
     (state);
 
--- 2.4 hospital_ownership table
+
+-- 2.4 zip_code table
+CREATE TABLE IF NOT EXISTS zip_code
+  ( zip_code_id INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
+    zip_code CHAR(10) NOT NULL UNIQUE,
+    PRIMARY KEY (zip_code_id)
+      )
+ENGINE=InnoDB
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_0900_ai_ci;
+
+LOAD DATA LOCAL INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/output/hosp_zip.csv'
+INTO TABLE zip_code
+  CHARACTER SET utf8mb4
+    FIELDS TERMINATED BY ','
+    ENCLOSED BY '"'
+    LINES TERMINATED BY '\n'
+    IGNORE 1 LINES
+    (zip_code);
+
+-- 2.5 hospital_ownership table
 
 CREATE TABLE IF NOT EXISTS hospital_ownership
   ( hospital_ownership_id INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
@@ -146,10 +167,15 @@ CREATE TABLE IF NOT EXISTS hospital (
   address VARCHAR(255) NOT NULL,
   city_id INTEGER NOT NULL,
   state_id INTEGER, 
-  zip_code CHAR(10) NOT NULL,
+  zip_code_id INTEGER NOT NULL,
   hospital_ownership_id INTEGER(5) NULL,
   hospital_quality_score_id INTEGER(5) NULL,
-  PRIMARY KEY (hospital_id)
+  PRIMARY KEY (hospital_id),
+  FOREIGN KEY (city_id) REFERENCES city(city_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (state_id) REFERENCES state(state_id) ON DELETE CASCADE ON UPDATE CASCADE,  
+  FOREIGN KEY (zip_code_id) REFERENCES zip_code(zip_code_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (hospital_ownership_id) REFERENCES hospital_ownership(hospital_ownership_id) ON DELETE CASCADE ON UPDATE CASCADE,  
+  FOREIGN KEY (hospital_quality_score_id) REFERENCES hospital_quality_score(hospital_quality_score_id) ON DELETE CASCADE ON UPDATE CASCADE
 )
 ENGINE=InnoDB
 CHARACTER SET utf8mb4
@@ -161,11 +187,11 @@ INSERT IGNORE INTO hospital (
       address,
       city_id,
       state_id,
-      zip_code,
+      zip_code_id,
       hospital_ownership_id,
       hospital_quality_score_id
 )
-SELECT th.hospital_provider_identifier, th.hospital_name, th.address, cit.city_id, s.state_id, th.zip_code, 
+SELECT th.hospital_provider_identifier, th.hospital_name, th.address, cit.city_id, s.state_id, z.zip_code, 
         ho.hospital_ownership_id, hq.hospital_quality_score_id
   FROM temp_hospital th
         LEFT JOIN city cit
@@ -175,8 +201,12 @@ SELECT th.hospital_provider_identifier, th.hospital_name, th.address, cit.city_i
         LEFT JOIN hospital_ownership ho
               ON TRIM(th.hospital_ownership) = TRIM(ho.hospital_ownership_description)
         LEFT JOIN hospital_quality_score hq
-              ON TRIM(th.hospital_quality_score = TRIM(hq.hospital_quality_score))
+              ON TRIM(th.hospital_quality_score) = TRIM(hq.hospital_quality_score)
+        LEFT JOIN zip_code z
+              ON TRIM(z.zip_code) = TRIM(th.zip_code)
 ORDER BY th.hospital_provider_identifier;
+
+
 
 -- THE FOLLOWING TABLES WORK DON'T TOUCH THEM
 -- 3.0 ENTITIES
@@ -186,79 +216,80 @@ ORDER BY th.hospital_provider_identifier;
 -- 12 columns total
 
 -- 3.1 temp_pricing
--- DROP TABLE temp_pricing;
--- CREATE TABLE IF NOT EXISTS temp_pricing (
---   pricing_id INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
---   drg_definition CHAR(250) NOT NULL,                   -- Put drg_definition before provider id because thats the order in the csv
---   pricing_provider_identifier CHAR(6) NOT NULL,
---   zip_code CHAR(10) NULL,
---   price DECIMAL(10,2) NULL,
---   PRIMARY KEY (pricing_id)
--- )
--- ENGINE=InnoDB
--- CHARACTER SET utf8mb4
--- COLLATE utf8mb4_0900_ai_ci;
 
--- LOAD DATA LOCAL INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/output/pricing_trimmed.csv' 
--- INTO TABLE temp_pricing
---   CHARACTER SET utf8mb4
---   FIELDS TERMINATED BY ','
---   ENCLOSED BY '"'
---   LINES TERMINATED BY '\n'
---   IGNORE 1 LINES
---   (drg_definition, pricing_provider_identifier, @dummy, @dummy, @dummy, @dummy, zip_code, 
---   @dummy, @dummy, price, @dummy, @dummy
---   );
+CREATE TABLE IF NOT EXISTS temp_pricing (
+  pricing_id INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
+  drg_definition CHAR(250) NOT NULL,                   -- Put drg_definition before provider id because thats the order in the csv
+  pricing_provider_identifier CHAR(6) NOT NULL,
+  zip_code CHAR(10) NULL,
+  price DECIMAL(10,2) NULL,
+  PRIMARY KEY (pricing_id)
+)
+ENGINE=InnoDB
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_0900_ai_ci;
+
+LOAD DATA LOCAL INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/output/pricing_trimmed.csv' 
+INTO TABLE temp_pricing
+  CHARACTER SET utf8mb4
+  FIELDS TERMINATED BY ','
+  ENCLOSED BY '"'
+  LINES TERMINATED BY '\n'
+  IGNORE 1 LINES
+  (drg_definition, pricing_provider_identifier, @dummy, @dummy, @dummy, @dummy, zip_code, 
+  @dummy, @dummy, price, @dummy, @dummy
+  );
 
 
 -- 3.2 charge_amount  this table is similar to the city table that has 1 entity that comes from its own csv
--- DROP TABLE charge_amount;
--- CREATE TABLE IF NOT EXISTS charge_amount (
---   charge_id INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
---   charge DECIMAL(10,2) NOT NULL UNIQUE,
---   PRIMARY KEY (charge_id)
--- )
--- ENGINE=InnoDB
--- CHARACTER SET utf8mb4
--- COLLATE utf8mb4_0900_ai_ci;
 
--- LOAD DATA LOCAL INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/output/price.csv'
--- INTO TABLE charge_amount
---   CHARACTER SET utf8mb4
---   FIELDS TERMINATED BY ','
---   ENCLOSED BY '"'
---   LINES TERMINATED BY '\n'
---   IGNORE 1 LINES
---   (charge);
+CREATE TABLE IF NOT EXISTS charge_amount (
+  charge_id INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
+  charge DECIMAL(10,2) NOT NULL UNIQUE,
+  PRIMARY KEY (charge_id)
+)
+ENGINE=InnoDB
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_0900_ai_ci;
+
+LOAD DATA LOCAL INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/output/price.csv'
+INTO TABLE charge_amount
+  CHARACTER SET utf8mb4
+  FIELDS TERMINATED BY ','
+  ENCLOSED BY '"'
+  LINES TERMINATED BY '\n'
+  IGNORE 1 LINES
+  (charge);
 
 
--- 3.3 pricing table  COMMENT THIS TABLE OUT IT WILL TRY TO CREATE A SECOND PRICING TABLE EVEN THOUGH IT SAYS IF NOT EXISTS AND IT TAKES FOREVER
--- DROP TABLE pricing;
--- CREATE TABLE IF NOT EXISTS pricing (
---   price_id INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
---   pricing_provider_identifier CHAR(6) NOT NULL UNIQUE,
---   charge_id INTEGER(10) NOT NULL,
---   drg_code VARCHAR(3) DEFAULT '313',
---   drg_definition CHAR(20) DEFAULT 'Chest Pain',
---   zip_code CHAR(10) NOT NULL,
---   PRIMARY KEY (price_id)
--- )
--- ENGINE=InnoDB
--- CHARACTER SET utf8mb4
--- COLLATE utf8mb4_0900_ai_ci;
+-- 3.3 pricing table 
 
--- INSERT IGNORE INTO pricing (
---       pricing_provider_identifier,
---       charge_id,
---       zip_code
--- )
--- SELECT tp.pricing_provider_identifier,
---        ch.charge_id, tp.zip_code
---   FROM temp_pricing tp
---        LEFT JOIN charge_amount ch
---               ON TRIM(tp.price) = TRIM(ch.charge)
---   WHERE tp.drg_definition = '313 - CHEST PAIN'
--- ORDER BY tp.pricing_provider_identifier;
+CREATE TABLE IF NOT EXISTS pricing (
+  price_id INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
+  pricing_provider_identifier CHAR(6) NOT NULL UNIQUE,
+  charge_id INTEGER(10) NOT NULL,
+  drg_code VARCHAR(3) DEFAULT '313',
+  drg_definition CHAR(20) DEFAULT 'Chest Pain',
+  zip_code CHAR(10) NOT NULL,
+  PRIMARY KEY (price_id),
+  FOREIGN KEY (charge_id) REFERENCES charge_amount(charge_id) ON DELETE CASCADE ON UPDATE CASCADE
+)
+ENGINE=InnoDB
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_0900_ai_ci;
+
+INSERT IGNORE INTO pricing (
+      pricing_provider_identifier,
+      charge_id,
+      zip_code
+)
+SELECT tp.pricing_provider_identifier,
+       ch.charge_id, tp.zip_code
+  FROM temp_pricing tp
+       LEFT JOIN charge_amount ch
+              ON TRIM(tp.price) = TRIM(ch.charge)
+  WHERE tp.drg_definition = '313 - CHEST PAIN'
+ORDER BY tp.pricing_provider_identifier;
 
 
 CREATE TABLE IF NOT EXISTS hospital_pricing (
